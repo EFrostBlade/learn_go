9288d425f9cd7152659cbf0c97ed6650e3832c06
EFrostBlade <3171264475@qq.com>
2022-04-11 21:11:11 +0800

    slices

--- arrays/sun_test.go
+++ arrays/sun_test.go
@@ -1,12 +1,25 @@
 package arrays
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestSum(t *testing.T) {
-	numbers := [5]int{1, 2, 3, 4, 5}
-	got := Sum(numbers)
-	want := 15
-	if want != got {
-		t.Errorf("Want %d but got %d, given %v", want, got, numbers)
-	}
+	/*t.Run("Collection of 5 numbers", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 4, 5}
+		got := Sum(numbers)
+		want := 15
+		if want != got {
+			t.Errorf("Want %d but got %d, given %v", want, got, numbers)
+		}
+	})
+	*/
+	t.Run("Collection of any nsize", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
+		got := Sum(numbers)
+		want := 6
+		if want != got {
+			t.Errorf("Want %d but got %d, given %v", want, got, numbers)
+		}
+	})
 }
